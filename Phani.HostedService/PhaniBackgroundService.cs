09d3de9cca84160829c42e63dfee41647e92571b
phani bandaru <pbndru@gmail.com>
2022-01-12 22:31:14 +0000

    Add project files.

---
+++ Phani.HostedService/PhaniBackgroundService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Phani.HostedService
+{
+    public class PhaniBackgroundService: BackgroundService
+    {
+        protected async override Task ExecuteAsync(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Console.WriteLine($"{ DateTime.Now.ToString() } - Phani Background Service");
+                await Task.Delay(5000, token);
+            }
+        }
+    }
+}
